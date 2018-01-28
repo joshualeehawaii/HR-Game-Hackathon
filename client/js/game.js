37e07b9546ae636339fd6fafd3530bd57db7b755
Joshua Lee <jdl93@nau.edu>
2018-01-27 17:33:36 -1000

    latests build with Dory's art

--- client/js/game.js
+++ client/js/game.js
@@ -8,13 +8,14 @@ function preload() {
   game.load.image('sky', 'assets/sky.png');
   game.load.image('ground', 'assets/platform.png');
   game.load.image('battery', 'assets/battery.png');
-  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.image('astroid', 'assets/astroid.png');
+  game.load.image('dude', 'assets/dude.png');
 }
 
 var player;
 var platforms;
 var cursors;
-
+var astroids;
 var batterys;
 var score = 0;
 var scoreText;
@@ -49,7 +50,9 @@ function create() {
   ledge.body.immovable = true;
 
   //The player and its settings
-  player = game.add.sprite(32, game.world.height - 150, 'dude');
+  player = game.add.sprite(100, 245, 'dude');
+
+  player.scale.setTo(0.3, 0.3);
 
   //Physics on the player
   game.physics.arcade.enable(player);
@@ -59,17 +62,13 @@ function create() {
   player.body.gravity.y = 300;
   player.body.collideWorldBounds = true;
 
-  //Two animations, walking left and right.
-  player.animations.add('left', [0, 1, 2, 3], 10, true);
-  player.animations.add('right', [5, 6, 7, 8], 10, true);
-
   //batterys to collect.
   batterys = game.add.group();
 
   //Physics for any battery that is created in this group.
   batterys.enableBody = true;
 
-  //12 of them evenly spaced apart
+  //Battery
   for (var i = 0; i < 1; i++) {
     //Create a battery inside of the 'batterys' group.
     var battery = batterys.create(i * 70, 0, 'battery');
@@ -79,6 +78,19 @@ function create() {
 
     //This just gives each battery a slightly random bounce value.
     battery.body.bounce.y = 0.7 + Math.random() * 0.2;
+  }
+
+  astroids = game.add.group();
+
+  astroids.enableBody = true;
+
+  //Here we'll create 12 of them evenly spaced apart.
+  for (var j = 0; j < 12; j++) {
+    //Create a astroid inside of the 'astroids' group.
+    var astroid = astroids.create(j * 70, 0, 'astroid');
+
+    //Let gravity do its thing.
+    astroid.body.gravity.y = 40;
   }
 
   //The score.
@@ -100,6 +112,8 @@ function update() {
 
   //Checks to see if the player overlaps with any of the batterys, if he does call the collectbattery function.
   game.physics.arcade.overlap(player, batterys, collectbattery, null, this);
+
+  game.physics.arcade.overlap(player, astroids, die, null, this);
 
   //Reset the players velocity (movement).
   player.body.velocity.x = 0;
@@ -134,4 +148,26 @@ function collectbattery(player, battery) {
   //Add and update the score.
   score = 'Battery Collected';
   scoreText.text = score;
+}
+
+function die(player, astroid) {
+  //Player dies and restarts the game.
+  player.kill();
+  astroid.kill();
+  create();
+
+  //Add and update the score.
+  score = 'Battery Collected';
+  scoreText.text = score;
+}
+
+function fallingAstroids() {
+  //Here we'll create 12 of them evenly spaced apart.
+  for (var k = 0; k < 12; k++) {
+    //Create a astroid inside of the 'astroids' group.
+    var astroid = astroids.create(k * 70, 0, 'astroid');
+
+    //Let gravity do its thing.
+    astroid.body.gravity.y = 100;
+  }
 }
