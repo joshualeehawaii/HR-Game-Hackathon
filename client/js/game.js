a52c873cddc73acd1a13b87244170de6ca7704cc
Joshua Lee <jdl93@nau.edu>
2018-01-27 22:35:31 -1000

    finished for the nght

--- client/js/game.js
+++ client/js/game.js
@@ -8,7 +8,6 @@ function preload() {
   game.load.image('sky', 'assets/sky.png');
   game.load.image('ground', 'assets/platform.png');
   game.load.image('battery', 'assets/battery.png');
-  game.load.image('astroid', 'assets/astroid.png');
   game.load.image('dude', 'assets/dude.png');
   game.load.image('lander', 'assets/lander.png');
 }
@@ -16,7 +15,6 @@ function preload() {
 var player;
 var platforms;
 var cursors;
-var astroids;
 var batterys;
 var score = 0;
 var scoreText;
@@ -24,26 +22,17 @@ var scoreText;
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  // BACKGROUND
   var sky = game.add.sprite(0, 0, 'sky');
   sky.alpha = 0.3;
 
   // PLATFORMS
   platforms = game.add.group();
   platforms.enableBody = true;
-
-  // GROUND
   var ground = platforms.create(0, game.world.height - 55, 'ground');
   ground.scale.setTo(1, 1);
   ground.body.immovable = true;
 
-  // //Two ledges.
-  // var ledge = platforms.create(400, 400, 'ground');
-  // ledge.body.immovable = true;
-  // ledge = platforms.create(-150, 250, 'ground');
-  // ledge.body.immovable = true;
-
-  // PLAYER
+  // PLAYERS
   player = game.add.sprite(800, 1, 'dude');
   player.scale.setTo(0.3, 0.3);
   game.physics.arcade.enable(player);
@@ -57,21 +46,11 @@ function create() {
   batterys.scale.setTo(1, 1);
   var battery = batterys.create(300, 345, 'battery');
 
-  // LANDER
+  // LANDERS
   landers = game.add.group();
   landers.enableBody = true;
   landers.scale.setTo(0.3, 0.3);
   var lander = landers.create(400, 500, 'lander');
-
-  // ASTROIDS
-  astroids = game.add.group();
-  astroids.enableBody = true;
-  astroids.scale.setTo(0.2, 0.2);
-  for (var j = 0; j < 2; j++) {
-    //Create a astroid inside of the 'astroids' group.
-    var astroid = astroids.create(j * 200, 0, 'astroid');
-    astroid.body.gravity.y = 40;
-  }
 
   // SCORE
   scoreText = game.add.text(16, 25, 'Collect Battery', {
@@ -86,67 +65,34 @@ function create() {
 }
 
 function update() {
-  //Collide the player and the batterys with the platforms.
   game.physics.arcade.collide(player, platforms);
   game.physics.arcade.collide(batterys, platforms);
-
-  //Checks to see if the player overlaps with any of the batterys, if he does call the collectbattery function.
   game.physics.arcade.overlap(player, batterys, collectbattery, null, this);
+  game.physics.arcade.overlap(player, batterys, null, this);
 
-  game.physics.arcade.overlap(player, astroids, die, null, this);
-
-  //Reset the players velocity (movement).
+  // RESET VELOCITY
   player.body.velocity.x = 0;
 
   if (cursors.left.isDown) {
-    //Move to the left.
     player.body.velocity.x = -150;
-
     player.animations.play('left');
   } else if (cursors.right.isDown) {
-    //Move to the right.
     player.body.velocity.x = 150;
-
     player.animations.play('right');
   } else {
-    //Stand still.
     player.animations.stop();
-
     player.frame = 4;
   }
 
-  //Allow the player to jump if they are touching the ground.
   if (cursors.up.isDown && player.body.touching.down) {
     player.body.velocity.y = -350;
   }
 }
 
 function collectbattery(player, battery) {
-  //Removes the battery from the screen.
   battery.kill();
 
-  //Add and update the score.
+  // UPDATE SCORE
   score = 'Battery Collected';
   scoreText.text = score;
-}
-
-function die(player, astroid) {
-  //Player dies and restarts the game.
-  player.kill();
-  astroid.kill();
-
-  //Add and update the score.
-  score = 'Battery Collected';
-  scoreText.text = score;
-}
-
-function fallingAstroids() {
-  //Here we'll create 12 of them evenly spaced apart.
-  for (var k = 0; k < 12; k++) {
-    //Create a astroid inside of the 'astroids' group.
-    var astroid = astroids.create(k * 70, 0, 'astroid');
-
-    //Let gravity do its thing.
-    astroid.body.gravity.y = 100;
-  }
 }
